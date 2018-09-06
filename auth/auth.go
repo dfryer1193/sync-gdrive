f2aefb285d450ce8ed4caadd7e457ddf44512d31
David Fryer <dfryer1193@gmail.com>
2018-09-06 19:14:14 -0400

    Create auth package
    Move auth.go to auth/ for auth package.

---
+++ auth/auth.go
@@ -0,0 +1,116 @@
+//
+// From: https://developers.google.com/drive/api/v3/quickstart/go
+//
+
+package auth
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+)
+
+type installed struct {
+	ClientID                string   `json:"client_id"`
+	ProjectID               string   `json:"project_id"`
+	AuthURI                 string   `json:"auth_uri"`
+	TokenURI                string   `json:"token_uri"`
+	AuthProviderX509CertURL string   `json:"auth_provider_x509_cert_url"`
+	ClientSecret            string   `json:"client_secret"`
+	RedirectURIs            []string `json:"redirect_uris"`
+}
+
+type credentials struct {
+	Installed installed `json:"installed"`
+}
+
+// Retrieve a token, save it, then return generated client
+func getClient(config *oauth2.Config) *http.Client {
+	tokenFile := "token.json"
+	tok, err := tokenFromFile(tokenFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokenFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser, then type the "+
+		"authorization code: \n%v\n", authURL)
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	json.NewEncoder(f).Encode(token)
+}
+
+// Authenticate with Google, and save the auth token.
+func Authenticate() {
+	b, err := ioutil.ReadFile("auth/client_id.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	client := getClient(config)
+
+	srv, err := drive.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+
+	// Get one file and print it
+	r, err := srv.Files.List().PageSize(1).
+		Fields("nextPageToken, files(id, name)").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve files %v", err)
+	}
+	fmt.Println("Files:")
+	if len(r.Files) == 0 {
+		fmt.Println("No files found.")
+	} else {
+		for _, i := range r.Files {
+			fmt.Printf("%v\n", i)
+		}
+	}
+}
--- gsync.go
+++
@@ -1,4 +0,0 @@
-package sync
-
-func main() {
-}
